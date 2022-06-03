18df5f08f3de97ebe09318ed3f9dfbbbd2678d46
TheDevJill <104874646+TheDevJill@users.noreply.github.com>
2022-06-02 19:06:55 -0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,53 @@
+# first_num = input("First number: ")
+# second_num = input("Second number: ")
+# sum = float(first_num) +float(second_num)
+# print(sum)
+
+# first_num = float(input("First number: "))
+# second_num = float(input("Second number: "))
+# sum = first_num + second_num
+# print("Sum: ", sum)
+
+# course = "Python for beginners"
+# print(course.replace("y", "4"))
+# print("Python" in course)
+
+# x = 10
+# x = x+3
+
+# x = 10
+# x += 3
+
+# x = 3!=2
+# print(x)
+
+# price = 5
+# print(not price>10)
+
+# how_much_love = 25
+#
+# if how_much_love > 30:
+#     print("too much love")
+# elif how_much_love > 20:
+#     print("goodbye")
+#
+# weight = float(input("Weight: "))
+# unit = input("(K)g or (L)b: ")
+
+# if unit.upper() == "L":
+#     print(weight)
+# elif unit.upper() == "K":
+#     print(float(weight*2.2))
+#conversion calc dont think form
+# if unit.upper() =="K":
+#     converted = weight / 0.45
+#     print("Weight in Kg: " + str(converted))
+# 
+# else:
+#     converted = weight * 0.45
+#     print("weight in lbs: " + str(converted))
+
+# -----while loops------
+
+i = 1
+whil
